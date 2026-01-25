bfd8e358b3cbfd601c8375df5b7757a74f0a6443
Francis Casao <franciskarlcasao@gmail.com>
2026-01-25 21:18:10 +0800

    Initial Commit

---
+++ manga-shark/manga_sharkApp.swift
@@ -0,0 +1,17 @@
+//
+//  manga_sharkApp.swift
+//  manga-shark
+//
+//  Created by Francis Casao on 1/25/26.
+//
+
+import SwiftUI
+
+@main
+struct manga_sharkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
